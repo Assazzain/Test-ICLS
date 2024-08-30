28a5d75e9b8de3e98368c7e5c4cf609a56be0604
Assazzain <96609553+Assazzain@users.noreply.github.com>
2024-08-30 20:32:37 +0700

    Add files via upload

---
+++ Soal_2_ILCS_BE/Soal_2_ILCS_BE/Soal_2_ILCS_BE/Controllers/DataPungutanController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Soal_2_ILCS_BE.Services;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace Soal_2_ILCS_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataPungutanController : ControllerBase
+    {
+        private readonly DataPungutanService _apiService;
+
+        public DataPungutanController(DataPungutanService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        [HttpGet("getdata")]
+        public async Task<IActionResult> GetData()
+        {
+            var jsonData = await _apiService.GetDataFromApi();
+
+            var data = JsonSerializer.Deserialize<dynamic>(jsonData);
+
+            if (data != null)
+            {
+                data.fob = data.nilai_incoterm + data.biaya_tambahan - data.biaya_pengurang + data.tarif_vd;
+
+                data.cif = data.fob + data.nilai_asuransi + data.freight;
+
+                data.cif_rp = data.CIF * data.nilai_kurs;
+            }
+
+            if (string.IsNullOrWhiteSpace(jsonData))
+            {
+                return NotFound(new { message = "Data tidak ditemukan." });
+            }
+            else
+            {
+                return Ok(data);
+            }
+        }
+
+
+    }
+}
---
+++ Soal_2_ILCS_BE/Soal_2_ILCS_BE/Soal_2_ILCS_BE/Services/DataPungutanService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Soal_2_ILCS_BE.Services
+{
+    public class DataPungutanService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataPungutanService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetDataFromApi()
+        {
+            var response = await _httpClient.GetStringAsync("https://api-hub.ilcs.co.id/test/v2/dataPungutan?id_aju=04eb6a72-bb63-5aed-5e92-f58a3bfd5da2");
+            return response;
+        }
+    }
+}
